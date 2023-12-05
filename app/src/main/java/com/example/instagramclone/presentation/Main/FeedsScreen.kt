e77535a69ad1682091b319e7e10e873898fa8837
Miguel Angel Islas Mendez <islasmiguel737@gmail.com>
2023-12-05 02:51:37 -0600

    06 Sexto Video

--- app/src/main/java/com/example/instagramclone/presentation/Main/FeedsScreen.kt
+++ app/src/main/java/com/example/instagramclone/presentation/Main/FeedsScreen.kt
@@ -1,8 +1,16 @@
 package com.example.instagramclone.presentation.Main
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -12,12 +20,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.example.instagramclone.presentation.BottomNavigationItem
 import com.example.instagramclone.presentation.BottomNavigationMenu
 import com.example.instagramclone.presentation.Main.Posts.PostViewModel
@@ -26,6 +37,7 @@ import com.example.instagramclone.util.Response
 import com.example.instagramclone.R
 import com.example.instagramclone.domain.model.Post
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FeedsScreen(navController:NavController){
@@ -71,8 +83,47 @@ fun FeedsScreen(navController:NavController){
 fun InstagramHomeContent(postsList:List<Post>) {
     LazyColumn{
         items(postsList, itemContent = {
-
+            InstagramListContent(it)
         })
+    }
+}
+
+@Composable
+fun InstagramListContent(it: Post) {
+    Column {
+        ProfileInfoSection(it)
+        PostImage(it)
+        PostDescription(it = it)
+    }
+}
+
+@Composable
+fun PostDescription(it: Post) {
+    Text(text = it.userName, style = MaterialTheme.typography.body1, modifier = Modifier
+        .padding(8.dp)
+        .fillMaxWidth()
+        .height(30.dp)
+    )
+}
+
+@Composable
+fun PostImage(it: Post) {
+    Icon(rememberImagePainter(data=it.postImage),contentDescription = null, modifier = Modifier
+        .fillMaxWidth()
+        .height(450.dp))
+
+}
+
+@Composable
+fun ProfileInfoSection(it: Post) {
+    Row (modifier= Modifier
+        .padding(8.dp)
+        .fillMaxWidth(), verticalAlignment = Alignment.CenterVertically){
+        Icon(rememberImagePainter(data=it.userImage),contentDescription = "ProfileImage", tint = Color.Unspecified)
+        Text(text = it.userName, style = MaterialTheme.typography.body1, modifier = Modifier
+            .padding(8.dp)
+            .weight(1f), textAlign = TextAlign.Left)
+        Icon(imageVector = Icons.Default.MoreVert, contentDescription = "null")
     }
 }
 
